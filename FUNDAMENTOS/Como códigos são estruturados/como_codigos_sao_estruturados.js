307705354435fe90921de1929d5e8746593b6238
Igor Vinicius A. Lima <87440985+igorlima2004@users.noreply.github.com>
2023-11-01 16:35:16 -0300

    Criada a Pasta de Fundamentos e lançado os dois primeiros exercícios
    Criada a pasta de fundamentos e lançado os exercícios de primeira aplicação e como os códigos são estruturados.

---
+++ FUNDAMENTOS/Como códigos são estruturados/como_codigos_sao_estruturados.js
@@ -0,0 +1,17 @@
+//instrução
+console.log("quebre 3 ovos")
+console.log("misture com farinha")
+console.log("leve ao forno")
+
+//Bloco de códigos
+{
+  console.log("coma 1 pedaço de pizza")
+}
+
+{
+  console.log("teste")
+  {
+     console.log("teste")
+     console.log("teste")
+  }
+}
