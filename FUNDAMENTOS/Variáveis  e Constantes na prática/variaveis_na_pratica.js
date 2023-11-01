9b923f61009cc31fc1f78d4cc026448b75795333
Igor Vinicius A. Lima <87440985+igorlima2004@users.noreply.github.com>
2023-11-01 16:57:46 -0300

    Exercícios Variáveis
    Lançado os exercícios sobre constantes, variáveis e seus tipos.

---
+++ FUNDAMENTOS/Variáveis  e Constantes na prática/variaveis_na_pratica.js
@@ -0,0 +1,13 @@
+let altura = 1.90
+let peso = 70
+
+const pi = 3.14
+
+//peso / (altura * altura)
+pi * 2
+
+/*
+1 ) não iniciadas com números, apenas letras ou _
+2) Não utilizar caracteres especiais como: ç ` ˜
+3) Não pode ser iguais as palavras reservadas
+*/
---
+++ FUNDAMENTOS/Variáveis e seus tipos/variaveis_seus_tipos.js
@@ -0,0 +1,7 @@
+let nome = "Igor" //String
+let numero = 1200// inteiros ou decimais
+let negativo = -1200
+
+let decimal = 1200.50
+
+let avancarProximoEpisodio = false // Booleanos
