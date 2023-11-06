6aff6f95b1efb2f87912a1eaf24126e1fac63d6f
Igor Vinicius A. Lima <87440985+igorlima2004@users.noreply.github.com>
2023-11-06 15:05:25 -0300

    Exercícios Operadors
    Feito os exercícios da aula operadores aritméticos, relacionais e lógicos.

---
+++ FUNDAMENTOS/Operadores Aritméticos/operadores_aritmeticos.js
@@ -0,0 +1,12 @@
+/* Operadores aritméticos
+* Somar (+)
+* Subtrair (-)
+* Multiplicar (*)
+* Dividir (/)
+*/
+let numero1 = 10
+let numero2 = 5
+
+let resultado = numero1 / numero2
+
+console.log(resultado)
---
+++ FUNDAMENTOS/Operadores Relacionais e Lógicos/operadores_relacionais_logicos.js
@@ -0,0 +1,35 @@
+/*
+* Operadores relacionais (retorna verdadeiro ou falso)
+== (igual a)
+!= (diferente)
+> (maior que)
+< (Menor que)
+>= (Maior ou igual)
+<= (Menor ou igual)
+*/
+let n1 = 20
+let n2 = 10
+//let idade = 15
+//let resultado = n1 <= n2
+//let resultado = idade > 18
+//let pedidoMinimo = 20
+//let compraPessoa = 5
+//let resultado = compraPessoa > pedidoMinimo
+//console.log(resultado)
+
+
+
+/*
+* Operadores lógicos (testa condições com true ou false)
+&& (e)
+|| (ou)
+*/
+//let idade = 15
+//let resultado = 2==3 || 1==3 
+//let resultado =  idade >= 18 && idade <= 40
+
+let idade = 18
+let totalCompra = 120
+
+let resultado =  idade>=40 || totalCompra >= 200
+console.log(resultado)
