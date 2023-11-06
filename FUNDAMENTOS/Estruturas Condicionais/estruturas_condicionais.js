ff643976b82721cede58d39c41d71a2edaeed780
Igor Vinicius A. Lima <87440985+igorlima2004@users.noreply.github.com>
2023-11-06 15:15:07 -0300

    Exercícios Estrutura Condicionais
    Feito os exercícios da aula sobre estruturas condicionais.

---
+++ FUNDAMENTOS/Estruturas Condicionais/estruturas_condicionais.js
@@ -0,0 +1,33 @@
+/*Estruturas condicionais if else
+- Verificar idade para entrar no site
+- Verificar se a compra é maior do que o mínimo
+- Verificar se tem acesso a recursos pagos
+*/
+
+/*
+if( idade >= 18 && idade <= 25){
+  console.log("Maior de idade")
+}
+*/
+/*
+let idade = 30
+let compras = 120
+
+if( idade >= 40 || compras >= 400 ){
+  console.log("Desconto de 20%")
+}else{
+  console.log("Não tem desconto")
+}
+*/
+
+let opcao = 0
+
+if( opcao == 1 ){
+  console.log("Saldo conta")
+}else if( opcao == 2){
+  console.log("Cartão de crédito")
+}else if( opcao == 3){
+  console.log("Descontos indevidos")
+}else{
+  console.log("Opção inválida")
+}
