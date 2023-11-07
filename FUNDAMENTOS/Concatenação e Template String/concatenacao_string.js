1c7711570568ff9f64887ddc56b95942d74f1052
Igor Vinicius A. Lima <87440985+igorlima2004@users.noreply.github.com>
2023-11-07 13:16:29 -0300

    Exercícios Concatenação
    Feito os exercícios sobre aula de concatenação e templates string.

---
+++ FUNDAMENTOS/Concatenação e Template String/concatenacao_string.js
@@ -0,0 +1,8 @@
+const nome = "Maria"
+const numero = 2
+
+//console.log( "Bem vindo " + nome + " você tem " + numero )
+
+const texto = `Bem vindo ${nome} 
+você tem ${numero}`
+console.log( texto )
