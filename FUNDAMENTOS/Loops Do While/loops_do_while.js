826c7515bebadfd17904e86d9384d809bc48d7ec
Igor Vinicius A. Lima <87440985+igorlima2004@users.noreply.github.com>
2023-11-07 13:26:56 -0300

    Exercícios Loops Do-While
    Feito os exercícios sobre a aula de loops do-while.

---
+++ FUNDAMENTOS/Loops Do While/loops_do_while.js
@@ -0,0 +1,43 @@
+//While
+/*let numero = 5
+while( numero >= 1 ){
+  console.log("Executou " + numero)
+  //numero = numero + 1
+  //numero = numero - 1
+  numero--
+}*/
+
+//do...while
+/*let numero = 0
+do{
+  console.log("Executou " + numero)
+  numero--
+}while( numero >= 1 )
+*/
+
+//for
+/*let numero = 5
+while( numero >= 1 ){
+  console.log("Executou " + numero)
+  //numero = numero + 1
+  //numero = numero - 1
+  numero--
+}*/
+let postagens = [
+    "Hoje passeando pela av paulista",
+    "Passeando em Campos do Jordão",
+    "Hoje fiz um curso de criação de sites",
+    "Na casa da mamãe, aproveitando o dia!",
+    "postagem 5"
+  ]
+  
+  for( indice in postagens ){
+    console.log(postagens[indice])
+  }
+  
+  /*
+  let total = postagens.length
+  for( let numero = 0; numero < total; numero++ ){
+    console.log(postagens[numero])
+  }*/
+  
