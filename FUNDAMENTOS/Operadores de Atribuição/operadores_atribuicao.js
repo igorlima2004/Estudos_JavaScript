ea8038bf0b37389fa8545b81b7171743617acbb1
Igor Vinicius A. Lima <87440985+igorlima2004@users.noreply.github.com>
2023-11-08 12:07:42 -0300

    Exercícios Operadores de Atribuição
    Feito os exercícios da aula de operadores de atribuição.

---
+++ FUNDAMENTOS/Operadores de Atribuição/operadores_atribuicao.js
@@ -0,0 +1,17 @@
+let salario = 500
+let bonus = 200
+
+//salario = salario + bonus
+//salario += bonus
+
+let desconto = 100
+//salario = salario - desconto
+//salario -= desconto
+
+let mult = 2
+//salario = salario * mult
+//salario *= mult
+
+let div = 2
+//salario = salario / div
+salario /= div
--- FUNDAMENTOS/Projeto Lista Nomes/js/script.js
+++ FUNDAMENTOS/Projeto Lista Nomes/js/script.js
@@ -1,1 +1,42 @@
-let noe
+let nomes = [
+    "igor",
+    "josé",
+    "letícia",
+    "maria",
+    "kauanny",
+    "carlos",
+    "pedro",
+    "carlos",
+    "marcela",
+]
+
+function pesquisarNome(){
+    let nomePesquisa = document.getElementById('campoNome').value
+    let intensLista = ''
+    for(indice in nomes){
+        if( nomePesquisa == nome){
+            intensLista += `
+            <li class="list-group-item">
+             ${nome}
+            </li>`
+        }
+
+    }
+}
+
+function carregarNomes(){
+    
+    let intensLista = ''
+    for(indice in nomes){
+
+        let nome = nomes[indice]
+        intensLista += `
+        <li class="list-group-item">
+         ${nome}
+        </li>`
+    }
+
+
+}
+
+document.getElementById('lista').innerHTML = intensLista
